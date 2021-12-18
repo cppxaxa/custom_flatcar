cd2ef49323e57938b72563801808aaf11fa2a256
Prateek Xaxa <cppxaxa@gmail.com>
2021-12-18 21:23:41 +0530

    Removed repo path dependency

--- unpack_repack.sh
+++ unpack_repack.sh
@@ -1,11 +1,17 @@
-rm -rf linux
+# Clean up
+# rm -rf linux
+rm -rf mnt
+rm -f new-flatcar.iso
 umount -f /mnt/linux
+
+# Ops
 sudo mkdir /mnt/linux
 sudo mount -o loop flatcar_production_iso_image.iso /mnt/linux/
-cd /mnt/
-tar -cvf - linux | (cd "/media/cppxaxa/New Volume/Ubuntu_Downloads/custom_flatcar_repo" && tar -xf - )
-cd "/media/cppxaxa/New Volume/Ubuntu_Downloads/custom_flatcar_repo/linux/"
-mkisofs -o ../new-flatcar.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V FLATCARNEW .
+tar -cvf - /mnt/linux | (tar -xf - )
+
+cd mnt/linux
+mkisofs -o ../../new-flatcar.iso -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -J -R -V FLATCARNEW .
+cd ../..
 
 # Dev
-cd "/media/cppxaxa/New Volume/Ubuntu_Downloads/custom_flatcar_repo"
+ls
